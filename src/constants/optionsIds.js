40487ae9000c8755c0985e92fb0a60cf407e7f6f
Wilmar Florez <wilmar.florez@elenas.la>
2024-07-10 14:27:37 -0500

    [progress] extract optionsIds

---
+++ src/constants/optionsIds.js
@@ -0,0 +1,6 @@
+module.exports = {
+  BOOK_ACCOMODATION: 'book_accomodation',
+  TOURIST_PLANS: 'tourist_plans',
+  CHECK_AVAILABILITY: 'check_availability',
+  TALK_TO_AN_AGENT: 'talk_to_an_agent',
+}
